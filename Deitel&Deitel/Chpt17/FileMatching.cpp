47287aeb4e52319b46d8a293bd94567e25f4f0f9
Joe-Demp <dempseyjoseph96@yahoo.com>
2019-09-22 13:37:28 +0100

    Committing all files from D&D Chpt18 - 22

--- Deitel&Deitel/Chpt17/FileMatching.cpp
+++ Deitel&Deitel/Chpt17/FileMatching.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <iomanip>
 using namespace std;
 
 #define SIZE 100
@@ -16,7 +17,11 @@ using namespace std;
 		lodge the new value of file i in newmast.dat
 */
 
-void initAccountFound(bool []);
+void initTransactionFound(int []);
+void recordTransactions(ifstream &, int []);
+void addValue(int [], int);
+void removeValue(int [], int);
+double scanTransactionFile(ifstream &, int);
 
 int main()
 {
@@ -30,43 +35,93 @@ int main()
 		exit(1);
 	}
 
-	bool accountFound[SIZE];
-	initAccountFound(accountFound);
+	cout << "Running file matching" << endl;
+
+	int TransactionFound[SIZE];
+	initTransactionFound(TransactionFound);
+	recordTransactions(inTransaction, TransactionFound); 	// Initial scan for found files
 
 	//	variables for oldmast
 	int accountNumber;
 	string name;
 	double currentBalance;
 
-	// variables for trans
+	outNewMaster.precision(2);
+
+	// File matching and updating
+	while (inOldMaster && !inOldMaster.eof())
+	{
+		inOldMaster >> dec >> accountNumber >> name >> currentBalance;
+		currentBalance += scanTransactionFile(inTransaction, accountNumber);
+		
+		// write the new values to the file
+		outNewMaster << fixed << showpoint << accountNumber << ' ' << name << ' ' << currentBalance << '\n';
+
+		// cancel the accountNumber's occurence in TransactionFound
+		removeValue(TransactionFound, accountNumber);
+	}
+
+	// Notify the user of TransactionFound values, flagging transactions that had no accounts
+	for (int i = 0; i < SIZE; ++i)
+		if (TransactionFound[i] > 0) 
+			cout << "No account number " << TransactionFound[i] 
+				<< " present in records. Transaction discarded" << endl;
+
+	cout << "File Matching Complete";
+}
+
+void initTransactionFound(int array[])
+{
+	for (int i = 0; i < SIZE; ++i)
+		array[i] = -1;
+}
+
+void recordTransactions(ifstream &input, int array[])
+{
 	int accountNum;
 	double dollarAmount;
 
-	inOldMaster >> accountNumber >> name >> currentBalance;
-	while (inOldMaster)
+	input.seekg(0);
+	int j = 0;
+	while (input && !input.eof())
 	{
-		// scan trans for relevant transactions
-		inTransaction.seekg(0);
-		while (inTransaction && !inTransaction.eof())
-		{
-			// do work
-			// i.e. compute new values for the file
-		}
-
-		// write the new values to the file
-
-		// take in a new file from oldmast
-		inOldMaster >> accountNumber >> name >> currentBalance;
+		input >> accountNum >> dollarAmount;
+		addValue(array, accountNum);
 	}
-
-	
-	inTransaction >> accountNum >> dollarAmount;
-
-	outNewMaster << accountNum << ' ' << name << ' ' << currentBalance;
 }
 
-void initAccountFound(bool array[])
+void addValue(int array[], int value)
 {
-	for (int i = 0; i < SIZE; ++i)
-		array[i] = false;
+	int i = 0;
+	while (i < SIZE && array[i] != value && array[i] != -1) ++i;
+	
+	if (i < SIZE && array[i] == -1)
+		array[i] = value;
+}
+
+void removeValue(int array[], int value)
+{
+	int i = 0;
+	while (i < SIZE && array[i] != value) ++i;
+
+	if (i < SIZE) array[i] = 0;
+}
+
+/* Function to scan the transaction file for a particular account and return changes*/
+double scanTransactionFile(ifstream &input, int accNo)
+{
+	int accountNum;
+	double dollarAmount;
+	double result = 0.0;
+
+	// scan trans for relevant transactions
+	input.seekg(0);
+	while (input && !input.eof())
+	{
+		// compute changes for the account
+		input >> accountNum >> dollarAmount;
+		if (accountNum == accNo)	// a transaction number = the account number
+			result += dollarAmount;
+	}
+	return result;
 }
